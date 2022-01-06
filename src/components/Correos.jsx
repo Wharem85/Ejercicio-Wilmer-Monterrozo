23bbfc12aae571c59b233172c8d5ac15c72587a5
Wharem85-25 <wharem85@hotmail.com>
2022-01-06 15:21:12 -0600

    se finalizo la parte de estadisticas de los mensajes

--- src/components/Correos.jsx
+++ src/components/Correos.jsx
@@ -35,25 +35,25 @@ const Correos = () => {
 				<img src={sent}></img>
 				<p>Mensajes enviados</p>
 				<p>25</p>
-				<div className="line-vertical"></div>
+				<div className="line-vertical line-sent"></div>
 			</div>
 			<div className="message-icons">
 				<img src={received}></img>
 				<p>Mensajes recibidos</p>
 				<p>25</p>
-				<div className="line-vertical"></div>
+				<div className="line-vertical line-received"></div>
 			</div>
 			<div className="message-icons">
 				<img src={notReceived}></img>
 				<p>Mensajes no recibidos</p>
 				<p>0</p>
-				<div className="line-vertical"></div>
+				<div className="line-vertical line-notReceived"></div>
 			</div>
-			<div className="message-icons">
+			<div className="message-icons message-answered">
 				<img src={answered}></img>
 				<p>Mensajes respondidos</p>
 				<p>0</p>
-				<div className="line-vertical"></div>
+				<div className="line-vertical line-answered"></div>
 			</div>
 		</div>
 	);
